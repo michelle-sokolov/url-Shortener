ab255dcb7c89488474a64f8e688d7e33f668e253
michelle-sokolov <michellersokolov@gmail.com>
2020-12-01 23:00:10 -0800

    route created

--- server.js
+++ server.js
@@ -1,4 +1,11 @@
 const express = require('express')
 const app = express()
 
+app.set('view engine', 'ejs')
+
+// route set up
+app.get('/', async (req, res) => {
+    res.render('index')
+})
+
 app.listen(process.env.PORT || 5000);
