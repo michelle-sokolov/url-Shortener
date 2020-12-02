85ab2959ac1376049ee44efe6a0989a7c04d9134
michelle-sokolov <michellersokolov@gmail.com>
2020-12-01 23:25:47 -0800

    change to server routes

--- server.js
+++ server.js
@@ -1,18 +1,24 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const ShortUrl = require('./models/shortUrl');
 const connectDB = require('./config/db');
 // connect to DB
 connectDB();
 
 app.set('view engine', 'ejs')
+// url parameters
+app.use(express.urlencoded({ extended: false }))
 
 // route set up
 app.get('/', async (req, res) => {
-    res.render('index')
+    // gets all urls 
+    const shortUrls = await ShortUrl.find()
+    // send short URLs to views
+    res.render('index', { shortUrls: shortUrls })
 })
-// POST request
-app.post('/shortUrls', (req, res) => {
-
+// POST request - create short URL
+app.post('/shortUrls', async (req, res) => {
+    await ShortUrl.create({ full: req.body.fullUrl })
 })
 
 app.listen(process.env.PORT || 5000);
